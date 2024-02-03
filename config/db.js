155e90581d886bceadb1e3c320597106f16900b0
Monisha <monisha.victor@ibm.com>
2024-02-03 10:54:22 +0530

    db config

--- config/db.js
+++ config/db.js
@@ -1,3 +1,22 @@
+let mongoDetails = { 
+	host: 'localhost',
+	port: '27017',
+	dbname: '',
+	user: '',
+	pass: ''
+}
+
+let variables = ['host', 'port', 'dbname', 'user', 'pass']
+variables.forEach((env) => {
+	mongoDetails[env] = process.env[env] ? process.env[env] : mongoDetails[env]
+})
+
+let url = `mongodb://${mongoDetails.host}:${mongoDetails.port}/${mongoDetails.dbname}`
+
+if (mongoDetails.user != '' && mongoDetails.pass != '') {
+
+}
+
 module.exports = {
-	url: "mongodb://localhost:27017/cloudsweep"
+	url: url
 };
